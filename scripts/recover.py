484aadd03ef0c1d33396a43d7efd0b549d6d0eed
wavey0x <retrowaveyy@gmail.com>
2022-11-28 14:48:58 -0500

    feat: log control

--- scripts/recover.py
+++ scripts/recover.py
@@ -1,4 +1,5 @@
 import json, telebot, os, time
+from datetime import datetime
 from dotenv import load_dotenv, find_dotenv
 from brownie import accounts, chain, Wei, Contract, ZERO_ADDRESS, web3
 
@@ -7,6 +8,8 @@ telegram_bot_key = os.environ.get('WAVEY_ALERTS_BOT_KEY')
 bot = telebot.TeleBot(telegram_bot_key)
 WALLET = '0x114Fe577BC999D7B854959859BfeA5CA0e0269D4'
 DEBT_REPAYER = '0x9eb6BF2E582279cfC1988d3F2043Ff4DF18fa6A0'
+SLEEP_SECONDS = 7
+LOG_ITERATIONS = 100
 
 TOKENS = {
     '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': {
@@ -41,15 +44,26 @@ def main():
     if is_dev:
         setup()
     repayer = Contract(DEBT_REPAYER, owner=wallet)
+    count = 0
     while True:
+        count += 1
+        if count % LOG_ITERATIONS == 0:
+            print_stuff = True
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print(f'--- {dt_string} ---')
+            print(f'Logging every {SLEEP_SECONDS * LOG_ITERATIONS / 60} minutes')
+        else:
+            print_stuff = False
         for address in TOKENS:
             balance = Contract(address).balanceOf(repayer)
             antoken = Contract(TOKENS[address]['antoken'])
             decimals = TOKENS[address]["decimals"]
             symbol = TOKENS[address]["symbol"]
             should_claim = balance > TOKENS[address]["threshold"]
-            print(f'\nCurrent contract {symbol} balance: {balance/10**decimals}',flush=True)
-            print(f'{"✅" if should_claim else "🟥"} {symbol} threshold: {TOKENS[address]["threshold"]/10**decimals}',flush=True)
+            if print_stuff:
+                print(f'Current contract {symbol} balance: {balance/10**decimals}',flush=True)
+                print(f'{"✅" if should_claim else "🟥"} {symbol} threshold: {TOKENS[address]["threshold"]/10**decimals}',flush=True)
             if should_claim:
                 an_balance = antoken.balanceOf(WALLET)
                 min = repayer.amountOut(antoken.address, address, an_balance)[0] * .99
@@ -58,12 +72,13 @@ def main():
                     repayment = tx.events['debtRepayment']
                     m = f'Received: {repayment["receiveAmount"]/10**decimals} {TOKENS[repayment["underlying"]]["symbol"]}\nPaid: {repayment["paidAmount"]/1e8} {antoken.symbol()}'
                     m += f'\n\n🔗 [View on Etherscan](https://etherscan.io/tx/{tx.txid})'
+                    print(m,flush=True)
                     send_alert(m)
                 except Exception as e:
                     print(e,flush=True)
                     m = f'Unable to send transaction for {symbol}.\n\nCurrent balance available: {balance/10**decimals}'
                     send_alert(m)
-        time.sleep(7)
+        time.sleep(SLEEP_SECONDS)
 
 def send_alert(m):
     bot.send_message('-789090497', m, parse_mode="markdown", disable_web_page_preview = True)
